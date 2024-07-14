8d9b1b3679ed2bffd0dc2b3933b568c0c43589e4
Ismoil <muhiddinovismoil2@gmail.com>
2024-07-14 14:44:10 +0500

    Masala yechildi va yangi fayl qo'shildi

---
+++ 1.py
@@ -0,0 +1,11 @@
+with open("raqamlar.txt", "w+") as f:
+    sum = 0
+    for x in range(1, 10 + 1):
+        f.write(str(x) + ' ')
+    print("Faylga 1-10 gacha bo'lgan sonlar yozildi")
+    f.seek(0)
+    res = f.read()
+    numbers = res.split()
+    for number in numbers:
+        sum += int(number)
+    print("Result =",sum)
